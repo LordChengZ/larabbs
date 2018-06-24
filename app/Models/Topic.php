f72a0f2c00a6ec989973df7d5893be8fe1d25521
LordCzH <673212712@qq.com>
2018-06-24 05:10:17 +0000

    Topic order

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -17,4 +17,30 @@ class Topic extends Model
     {
     	return $this->belongsTo(User::class);
     }
+
+    public function scopeWithOrder($query, $order)
+    {
+    	//用范围函数 scope 定义的时候，实例化调用的时候，去掉scope，然后首字母小写
+    	switch ($order) {
+    		case 'recent':
+    			$query->recent();
+    			break;
+    		
+    		default:
+    			$query->recentReplied();
+    			break;
+    	}
+
+    	return $query->with('user', 'category');
+    }
+
+    public function scopeRecentReplied($query)
+    {
+    	return $query->orderBy('updated_at', 'desc');
+    }
+
+    public function scopeRecent($query)
+    {
+    	return $query->orderBy('created_at', 'desc');
+    }
 }
